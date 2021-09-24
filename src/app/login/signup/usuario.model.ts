bf69c42c13720e9d7510b7dec041203157a624eb
Sebastian Miranda <SebaMSH>
2021-09-24 01:54:14 -0300

    Registro Completado

---
+++ src/app/login/signup/usuario.model.ts
@@ -0,0 +1,5 @@
+export interface Usuario {
+    name: string;
+    user: string;
+    password: string;
+};
---
+++ src/app/login/signup/usuario.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from './usuario.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  listaUsuario: Usuario[]=[
+    {
+      name: 'Marcelo',
+      user: 'mmonte',
+      password: '123456'
+    },
+  ];
+  constructor() { }
+  getUsuario(usuarioInput: string)
+  {
+    return {
+            ...this.listaUsuario.find(usuario => {return usuario.user === usuarioInput })
+           }
+  }
+  addUsuario(name: string, user: string, password: string)
+  {
+    this.listaUsuario.push(
+      {
+        name,
+        user,
+        password
+
+      }
+    );   
+  }
+}
+
