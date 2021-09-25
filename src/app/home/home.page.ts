3b7d1cf81bc5a43803a34a1ee57ceca5c4c045a0
unknown <SebaMSH>
2021-09-25 11:45:41 -0300

    Bienvenido: user

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
+export class HomePage implements OnInit{
+  user: '';
+  constructor(private activatedRoute: ActivatedRoute, private router : Router) {}
+  ngOnInit(){
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.user = this.router.getCurrentNavigation().extras.state.user.usuario;
+  });
+  }
 
 }
